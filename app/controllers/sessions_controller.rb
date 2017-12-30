c43456cf81ada64ff01963304bedc8a056a7933e
kakko341 <riimoh3@yahoo.co.jp>
2017-12-30 13:56:37 +0000

    icon

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,13 @@ class SessionsController < ApplicationController
     email = params[:session][:email].downcase
     password = params[:session][:password]
     if login(email, password)
-      flash[:success] = "ログインに成功しました。"
+      flash[:success] = 'ログインに成功しました。'
       redirect_to @user
     else
-      flash.now[:danger] = "ログインに失敗しました。"
-      render new
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
     end
+    
   end
 
   def destroy
